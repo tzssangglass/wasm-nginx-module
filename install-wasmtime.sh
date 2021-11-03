2c7f6dd9643990fe40ffb5b232d5beefc3ee0afb
tzssangglass <tzssangglass@gmail.com>
2021-11-03 16:04:40 +0800

    fix: build on macos x86_64
    Signed-off-by: tzssangglass <tzssangglass@gmail.com>

--- install-wasmtime.sh
+++ install-wasmtime.sh
@@ -2,9 +2,9 @@
 set -euo pipefail -x
 
 VER=v0.30.0
-wget https://github.com/bytecodealliance/wasmtime/releases/download/${VER}/wasmtime-${VER}-x86_64-linux-c-api.tar.xz
-tar -xvf ./wasmtime-${VER}-x86_64-linux-c-api.tar.xz > /dev/null
-mv wasmtime-${VER}-x86_64-linux-c-api wasmtime-c-api
+wget https://github.com/bytecodealliance/wasmtime/releases/download/${VER}/wasmtime-${VER}-x86_64-macos-c-api.tar.xz
+tar -xvf ./wasmtime-${VER}-x86_64-macos-c-api.tar.xz > /dev/null
+mv wasmtime-${VER}-x86_64-macos-c-api wasmtime-c-api
 if echo "int main(void) {}" | gcc -o /dev/null -v -x c - &> /dev/stdout| grep collect | tr -s " " "\012" | grep musl; then
     # build from source code if the libc is musl
     git clone https://github.com/bytecodealliance/wasmtime -b ${VER} --depth 1 \
